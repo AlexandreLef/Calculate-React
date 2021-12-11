f50e1625fb2e846aa9469926ff231ab1c6102eca
Quentin Villanova <villanovaq@gmail.com>
2021-12-11 10:21:32 +0100

    Design WIP

--- calculate/src/App.tsx
+++ calculate/src/App.tsx
@@ -2,43 +2,14 @@ import React from 'react';
 import './App.css';
 import Calculate from './Calculate';
 import './bootstrap/bootstrap.min.css';
-import Settings from './Settings';
-import {ThemeContext, themes} from './ThemeContext';
 
-type themeProps = {
-  changeTheme: string
-}
-
-type themeState = {
-  theme: any
-}
-
-class App extends React.Component<themeProps, themeState> {
-  toggleTheme: () => void;
-  constructor(props: themeProps | Readonly<themeProps>) {
-    super(props);
-    this.state = {
-      theme: themes,
-    };
-
-    this.toggleTheme = () => {
-      this.setState(state => ({
-        theme:
-          state.theme === themes.dark
-            ? themes.light
-            : themes.dark,
-      }));
-    };
-  }
+class App extends React.Component {
   
   render() {
     return (
-      <ThemeContext.Provider value={this.state.theme}>
         <div className="container">
           <Calculate />
-            <Settings onClick={this.props.changeTheme}/>
         </div>
-      </ThemeContext.Provider>
     );
   }
 }
--- calculate/src/index.tsx
+++ calculate/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App changeTheme="dark"/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
